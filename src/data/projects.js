21f9b93841bd0393bd610f850ce61c656683a32a
Théodore Taymans <theodore@l-union.be>
2026-05-28 14:34:46 +0200

    Dynamic slugs + reusable architecture cleanup

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 export const projects = [
   {
     title: "Be Heroes",
+slug: "be-heroes",
     image:
       "https://images.unsplash.com/photo-1518005020951-eccb494ad742?q=80&w=1200",
     category: "Campaign • Branding",
@@ -9,7 +10,8 @@ export const projects = [
   },
 
   {
-    title: "Beobank",
+    ttitle: "Beobank",
+  slug: "beobank",
     image:
       "https://images.unsplash.com/photo-1497366754035-f200968a6e72?q=80&w=1200",
     category: "Digital • UX/UI",
@@ -19,6 +21,7 @@ export const projects = [
 
   {
     title: "Proximus Event",
+  slug: "proximus-event",
     image:
       "https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=1200",
     category: "Events • Production",
